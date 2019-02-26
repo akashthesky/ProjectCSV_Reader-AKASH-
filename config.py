a8ba6cbb42404f52213b81c9c0ecafc30193295f
Akash Dey <42717629+akashthesky@users.noreply.github.com>
2019-02-26 13:14:22 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+table_name="Electricity"
+file_name="data.txt"
+thisdict =	{
+  0: "DATE",
+  1: "TIME",
+  2: "DECIMAL(4,3)",
+  3: "DECIMAL(4,3)",
+  4: "DECIMAL(6,3)",
+  5: "DECIMAL(5,3)",
+  6: "DECIMAL(4,3)",
+  7: "DECIMAL(4,3)",
+  8: "DECIMAL(5,3)"
+}
