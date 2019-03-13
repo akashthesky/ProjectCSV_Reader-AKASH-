3a5c21adb91aca1f8a3afe4d9bafca13c1a4d818
Akash Dey <42717629+akashthesky@users.noreply.github.com>
2019-03-13 14:36:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,24 @@
+'''
+Created on Mar 12, 2019
+
+@author: akash18.TRN
+'''
 dbname="Hello"
 tname="employee"
-filename="data.txt"
+filename="data2.txt"
 primarykey_col=-1
 thisdict={
     
 };
+
+datatype_dict={
+    1: "date",
+    2: "time",
+    3: "DECIMAL(9,3)",
+    4: "DECIMAL(9,3)",
+    5: "DECIMAL(9,3)",
+    6: "DECIMAL(9,3)",
+    7: "DECIMAL(9,3)",
+    8: "DECIMAL(9,3)",
+    9: "DECIMAL(9,3)"
+    };
