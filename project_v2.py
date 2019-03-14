810d402e00867cc6c6c6b172a835b2eb10a435e3
Akash Dey <42717629+akashthesky@users.noreply.github.com>
2019-03-14 16:27:57 +0530

    Update project_v2.py

--- project_v2.py
+++ project_v2.py
@@ -1,4 +1,4 @@
-from config import *
+
 from datetime import datetime
 import time
 import MySQLdb
@@ -57,12 +57,15 @@ class Table_Create():
         print(str1)
     
     def extractcol(self):
-        with open(self.fname, "r") as file:
-            read = csv.reader(file, delimiter=";")
-            for row in read:
-                for i in row:
-                    self.rows.append(i)
-                break
+        if(choice==2):
+            self.rows=col_name
+        if(choice==1):
+            with open(self.fname, "r") as file:
+                read = csv.reader(file, delimiter=deli)
+                for row in read:
+                    for i in row:
+                        self.rows.append(i)
+                    break
     
     def datatype(self):
         for k in datatype_dict.keys():
@@ -86,13 +89,15 @@ class Insert_Data():
     def insertfromcsv(self):
         usedb = "use " + dbname + ";"
         self.Start_Connection_Object.cursor.execute(usedb)
+        count = 1
         with open(filename) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=';')
-            count = 1
+            csv_reader = csv.reader(csv_file, delimiter=deli)
+            
             final_qry = ""
             qry = ""
-        
-            column_name = []
+            if choice==2:
+                count=2
+            
             insert_qry = "insert into " + tname + " values"
             for i in csv_reader:
                 index = 0
@@ -106,8 +111,14 @@ class Insert_Data():
                         else:
                             flag = 0
                             mat = re.match('(\d{1,2})[/.-](\d{1,2})[/.-](\d{4})$', j)
+                            bat = re.match('(\d{1,2})[/.-](\d{1,2})[/.-](\d{4}) (\d{1,2}):*', j)
                             if mat is not None:
                                 j = datetime.strptime(j, "%d/%m/%Y").strftime('%Y-%m-%d')
+                            if bat is not None:
+                                datetime_generate_obj=datetime_generate()
+                                a=datetime_generate_obj.get_datetime_string(j)
+                                j=str(a)
+                                
                             if (j == '' or j == ' ' or j == '?'):
                                 key = self.Table_Create_Object.get_index_column(index)
                                 j = thisdict[key]
@@ -123,29 +134,32 @@ class Insert_Data():
                             final_qry = final_qry[0:len(final_qry) - 3] + ")"
                         
                     insert_qry += final_qry + ","
-                
-                if(count % 20000 == 0 and count > 0):
+                #print(insert_qry)
+                if(count % batch_size == 0 and count > 0):
                     insert_qry = insert_qry[0:len(insert_qry) - 2] + ")"
-                    try:
-                        self.Start_Connection_Object.cursor.execute(insert_qry)
-                        self.Start_Connection_Object.db1.commit()
-                    except:
-                        f = open("errorlog.log", "a")
-                        f.write("Error in Batch  !! " + count%2000 + "\n")
+                    self.Start_Connection_Object.cursor.execute(insert_qry)
+                    self.Start_Connection_Object.db1.commit()
                     insert_qry = "insert into " + tname + " values"
                     qry = ""
-                    print("Inserting In Batches " + str(count // 20000))
+                    print("Inserting In Batches " + str(count // batch_size))
                 count += 1
         insert_qry = insert_qry[0:len(insert_qry) - 2] + ")"
-        try:
-            self.Start_Connection_Object.cursor.execute(insert_qry)
-            self.Start_Connection_Object.db1.commit()
-        except:
-            f = open("errorlog.log", "a")
-            f.write("Error in Batch  !! " + (count%2000)+1 + "\n")
+
+        self.Start_Connection_Object.cursor.execute(insert_qry)
+        self.Start_Connection_Object.db1.commit()
         print("Insert Finished")
         self.Start_Connection_Object.cursor.close()
         # print(insert_qry)
+
+
+class datetime_generate:
+    def get_datetime_string(self,str):
+        input_datetime=str
+        date_input=datetime.strptime(input_datetime[0:10], "%d.%m.%Y").strftime('%Y-%m-%d')
+        date_input=date_input+"T"+input_datetime[11:19]+'.'+input_datetime[20:]
+        regxf = "%Y-%m-%dT%H:%M:%S.%f"
+        out = datetime.strptime(date_input, regxf)
+        return out
 
 
 class SetNullValue:
@@ -165,12 +179,12 @@ class SetNullValue:
             return 'None'
         elif (i == 'int'):
             return '0'
+        elif (i[0:8] == 'datetime'):
+            return 'null'
+        elif (i == 'double'):
+            return '0'
         elif (i[0:7] == 'DECIMAL'):
-            p = int(i[8])
-            d = int(i[10])
-            val = '0' * (p - d)
-            val = val + '.' + '0' * d
-            return val
+            return '0.0'
         elif (i == 'date'):
             return 'null'
         elif (i == 'time'):
@@ -179,6 +193,17 @@ class SetNullValue:
             return 'None'
 
 
+print("***PROJECT MENU***\n")
+print("1: CSV With Coulmn Name\n")
+print("2: CSV With-Out Coulmn Name\n")
+choice=int(input("Enter The Choice \n"))
+if choice==1:
+    from config import *
+elif choice==2:
+    from config2 import *
+else:
+    print("Wrong Choice\n")
+    exit()
 
 f = open("errorlog.log", "w")
 f.write("Issues :            Time \n")
